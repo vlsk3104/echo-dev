63f416044323e8321f407816b360be1e882ce526
vlsk3104 <vlsk3104@gmail.com>
2025-09-28 00:39:41 +0900

    fix

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -11,9 +11,9 @@ const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
       <WidgetHeader>
-        <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-          <p>こんにちは 👋</p>
-          <p>本日はどのようなご用件でしょうか？</p>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+          <p className="text-3xl">こんにちは 👋</p>
+          <p className="text-lg">本日はどのようなご用件でしょうか？</p>
         </div>
       </WidgetHeader>
       <div className="flex flex-1">WidgetView: {organizationId}</div>
